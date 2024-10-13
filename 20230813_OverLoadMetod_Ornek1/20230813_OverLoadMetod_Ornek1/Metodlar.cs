a91ea785b54a16bead822b751eea92bb93d1ca2a
sude <sudefidan@icloud.com>
2024-10-13 18:55:32 +0300

    overloadmetod-ornek1

---
+++ 20230813_OverLoadMetod_Ornek1/20230813_OverLoadMetod_Ornek1/Metodlar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230813_OverLoadMetod_Ornek1
+{
+    internal class Metodlar
+    {
+        public void Hesapla(int s1,int s2)
+        {
+            Console.WriteLine("Geçerli Bir işlem Türü Girmediniz Sayılar Toplanacak ");
+            Console.WriteLine("Sayıların Toplamı = "+(s1+s2));
+        }
+
+        public void Hesapla(int s1,int s2 ,string secim)
+        {
+            string format = "{0} {1} {2} = {3}";
+            double sonuc = 0;
+            switch (secim)
+            {
+                case "+":
+                    sonuc = s1 + s2;
+                    break;
+
+                case "-":
+                    sonuc = s1 - s2;
+                    break;
+
+                case "*":
+                    sonuc = s1 * s2;
+                    break;
+
+                case "/":
+                    sonuc = s1 / s2;
+                    break; 
+            }
+            Console.WriteLine(format,s1,secim,s2,sonuc);
+        }
+    }
+}
---
+++ 20230813_OverLoadMetod_Ornek1/20230813_OverLoadMetod_Ornek1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230813_OverLoadMetod_Ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ornek 1 
+            //Kullanıcıdan 2 adet sayı talep edelim
+            //Bir adette işlem türü talep edelim
+            //İşlem türüne göre sonucu ekrana yazdıralım
+            //Kullanıcı işlem türü girmeden enter ' a basarsa toplama işlemi yaptıralım
+
+            Console.Write("1.Sayıyı Girin : ");
+            int sayi1= int.Parse(Console.ReadLine());
+
+            Console.Write("2.Sayıyı Girin : ");
+            int sayi2=int.Parse(Console.ReadLine());
+
+            Console.Write("İşlem Türünü Giriniz (+ , - , * , / ) : ");
+            string islemTuru=Console.ReadLine();
+
+            Metodlar m=new Metodlar();
+            if (string.IsNullOrEmpty(islemTuru))
+            {
+                m.Hesapla(sayi1,sayi2);
+            }
+            else 
+            {
+                m.Hesapla(sayi1,sayi2,islemTuru);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
